d0780b4ce091ab7e0e089e3a9312b8730ae4fcb4
kajal singh <133157917+kajalsingh8@users.noreply.github.com>
2024-12-15 12:25:24 +0530

    Add files via upload

---
+++ utils/logger.go
@@ -0,0 +1,13 @@
+package utils
+
+import (
+	"os"
+
+	"github.com/sirupsen/logrus"
+)
+
+func InitLogger() {
+	logrus.SetFormatter(&logrus.JSONFormatter{})
+	logrus.SetOutput(os.Stdout)
+	logrus.SetLevel(logrus.InfoLevel)
+}
---
+++ utils/response.go
@@ -0,0 +1,11 @@
+package utils
+
+import "github.com/gin-gonic/gin"
+
+func RespondJSON(c *gin.Context, status int, payload interface{}) {
+	c.JSON(status, gin.H{"data": payload})
+}
+
+func RespondError(c *gin.Context, status int, message string) {
+	c.JSON(status, gin.H{"error": message})
+}
